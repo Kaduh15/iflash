4c36f99ca804ae649f24bdd0b1c861367634d0a3
Kaduh15 <kadu.silva2014@gmail.com>
2025-10-22 14:03:58 -0300

    feat: migração de React para Angular

--- apps/web/src/routes/_public/auth/sing-in.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/_public/auth/sing-in')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/_public/auth/sing-in"!</div>
-}
